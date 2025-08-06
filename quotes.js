a0330d11f29602468346b80df73b11327933bbdc
Daria Bannikova <dbdadika@gmail.com>
2025-08-06 16:02:24 -0400

    Splitting app into the JS modules

---
+++ quotes.js
@@ -0,0 +1,22 @@
+const quotes = [
+  {
+    quote:
+      "The only limit to our realization of tomorrow will be our doubts of today",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    quote: "The only way to do great work is to love what you do",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "You are never too old to set another goal or to dream a new dream",
+    author: "C.S. Lewis",
+  },
+  {
+    quote:
+      "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart",
+    author: "Helen Keller",
+  },
+];
+
+export default quotes;
--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-const quotes = [
-  {
-    quote:
-      "The only limit to our realization of tomorrow will be our doubts of today",
-    author: "Franklin D. Roosevelt",
-  },
-  {
-    quote: "The only way to do great work is to love what you do",
-    author: "Steve Jobs",
-  },
-  {
-    quote: "You are never too old to set another goal or to dream a new dream",
-    author: "C.S. Lewis",
-  },
-  {
-    quote:
-      "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart",
-    author: "Helen Keller",
-  },
-];
+import quotes from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quote-author");

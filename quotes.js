99cc09778411b6b4105b36c3ecfb5af79b3baa41
Daria Bannikova <dbdadika@gmail.com>
2025-08-07 00:41:40 -0400

    Show favorite quotes below current quote

--- quotes.js
+++ quotes.js
@@ -3,19 +3,23 @@ const quotes = [
     quote:
       "The only limit to our realization of tomorrow will be our doubts of today",
     author: "Franklin D. Roosevelt",
+    isFavorite: false,
   },
   {
     quote: "The only way to do great work is to love what you do",
     author: "Steve Jobs",
+    isFavorite: false,
   },
   {
     quote: "You are never too old to set another goal or to dream a new dream",
     author: "C.S. Lewis",
+    isFavorite: false,
   },
   {
     quote:
       "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart",
     author: "Helen Keller",
+    isFavorite: false,
   },
 ];
 

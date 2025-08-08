fb7134071609b9513b5bee09fdaa9fb020d1b143
Daria Bannikova <dbdadika@gmail.com>
2025-08-08 17:53:13 -0400

    Refactoring - create favorites and quote modules

---
+++ src/data/quotes.js
@@ -0,0 +1,24 @@
+const quotes = [
+  {
+    text: "The only limit to our realization of tomorrow will be our doubts of today",
+    author: "Franklin D. Roosevelt",
+    isFavorite: false,
+  },
+  {
+    text: "The only way to do great work is to love what you do",
+    author: "Steve Jobs",
+    isFavorite: false,
+  },
+  {
+    text: "You are never too old to set another goal or to dream a new dream",
+    author: "C.S. Lewis",
+    isFavorite: false,
+  },
+  {
+    text: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart",
+    author: "Helen Keller",
+    isFavorite: false,
+  },
+];
+
+export default quotes;
